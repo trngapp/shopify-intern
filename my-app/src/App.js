766dad8f4e6689f9e7969d383461426eee1e6f78
tarang sharma <74776793+trngapp@users.noreply.github.com>
2022-05-22 22:25:33 +0530

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { Routes, Route } from "react-router-dom";
+//import { Routes, Route } from "react-router-dom";
 import Home from "../src/components/home"
 function App() {
   return (
